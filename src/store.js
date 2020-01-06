e2a1110cf5c9688db57897592a45828ee4df380b
elsayed1 <elsayed_alaa70@yahoo.com>
2020-01-06 03:30:23 +0200

    home page

--- src/store.js
+++ src/store.js
@@ -6,10 +6,8 @@ const middleware = [thunk];
 const store = createStore(
   reducers,
   {},
-  compose(
-    applyMiddleware(...middleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  )
+
+  applyMiddleware(...middleware)
 );
 
 export default store;
