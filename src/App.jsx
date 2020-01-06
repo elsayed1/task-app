b86e5bdc988828166be97d5f02e9223d01666784
elsayed1 <elsayed_alaa70@yahoo.com>
2020-01-06 15:50:25 +0200

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,32 @@
 import React, { Component } from "react";
-// import SideMenu from "./components/sideMenu/SideMenu";
-// import "./index.css";
-// import Navbar from "./components/navBar/Navbar";
-// import Exams from "./components/exams/Exams";
-// import Announcements from "./components/announcements/Announcements";
-// import Quiz from "./components/quiz/Quiz";
-// import store from "../src/store";
+import SideMenu from "./components/sideMenu/SideMenu";
+import "./index.css";
+import Navbar from "./components/navBar/Navbar";
+import Exams from "./components/exams/Exams";
+import Announcements from "./components/announcements/Announcements";
+import Quiz from "./components/quiz/Quiz";
+import store from "../src/store";
 
-// import { Provider } from "react-redux";
+import { Provider } from "react-redux";
 
 export default class App extends Component {
   static propTypes = {};
 
   render() {
     return (
-      <h1>test</h1>
-      // <Provider store={store}>
-      //   <div className="app">
-      //     <SideMenu />
-      //     <div className="content">
-      //       <Navbar />
-      //       <Exams />
-      //       <div className="frames">
-      //         <Announcements />
-      //         <Quiz />
-      //       </div>
-      //     </div>
-      //   </div>
-      // </Provider>
+      <Provider store={store}>
+        <div className="app">
+          <SideMenu />
+          <div className="content">
+            <Navbar />
+            <Exams />
+            <div className="frames">
+              <Announcements />
+              <Quiz />
+            </div>
+          </div>
+        </div>
+      </Provider>
     );
   }
 }
